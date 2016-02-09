6fd6288dbc92df3131a7fe0bc9f30050e0b7738a
Tina Fan <tina.fan@live.com>
2016-02-08 17:07:31 -0800

    Added authentication (still needs improvement)

--- routes/index.ts
+++ routes/index.ts
@@ -56,6 +56,49 @@ router.get('/userlist', function(req, res) {
 });
 
 
+/* POST to Authenticate Service */
+router.post('/login', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userPassword = req.body.userpassword;
+    console.log(userName);
+    console.log(userPassword);
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    collection.findOne({
+        $or:[
+            {"email": userName},
+            {"username": userName}
+        ]
+    }, function(err, user) {
+        if(err) {
+         console.log("hi4");
+            res.send("There was an error");
+        }
+        else if(!user) {
+            console.log("hi1");
+            res.send("No such user exists");
+        }
+        else if(user.password != userPassword){
+            console.log("hi2");
+            res.send("Incorrect Password");
+            console.log(user["password"]);
+            console.log(user.password);
+        } else {
+            console.log(user["password"]);
+
+            res.redirect("home");
+            console.log(user);
+        }
+    });
+
+});
 
 /* POST to Add User Service */
 router.post('/adduser', function(req, res) {
@@ -66,22 +109,43 @@ router.post('/adduser', function(req, res) {
     // Get our form values. These rely on the "name" attributes
     var userName = req.body.username;
     var userEmail = req.body.useremail;
+    var userPassword = req.body.userpassword;
 
     // Set our collection
     var collection = db.get('usercollection');
 
     // Submit to the DB
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
+    collection.findOne({
+
+        $or:[
+            {"email": userEmail},
+            {"username": userName}
+        ]
+    
+    }, function(err, user) {
+        
+        if(err) {
+            res.send("There was an error");
         }
-        else {
-            // And forward to success page
-            res.redirect("userlist");
+        else if(!user) {
+
+            collection.insert({
+                "username" : userName,
+                "email" : userEmail,
+                "password" : userPassword
+            }, function (err, user) {
+                if (err) {
+                    // If it failed, return error
+                    res.send("Database Insert Failed");
+                }
+                else {
+                    // And forward to success page
+                    res.redirect("login");
+                }
+            });
+
+        } else {
+            res.redirect("Username or Email is already taken");
         }
     });
 });
