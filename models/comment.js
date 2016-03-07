3d5b6ead2e810f462e6050144432c0b13e0db26a
Bryan Ng <bryan.yc.ng@shaw.ca>
2016-03-06 21:22:29 -0800

    Create comment.js
    Added this comment model for comments, not sure if we'll change our whole models or not

---
+++ models/comment.js
@@ -0,0 +1,19 @@
+/**
+ * This is the model for the json string that will be saved in our database
+ */
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var commentSchema = mongoose.Schema({
+    comment: {
+        post: String,
+        commentor: String,
+        picture: String,
+        date: String,
+    }
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Comment', commentSchema);
