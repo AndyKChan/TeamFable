1cc2fab3631c96add1f0e0e41a50e6945203866a
AndyKChan <r3v8@ugrad.cs.ubc.ca>
2016-03-26 15:45:12 -0700

    hi

--- app.js
+++ app.js
@@ -25,44 +25,82 @@ var Application = (function () {
         var app = express();
         var multer = require('multer');
         //Setting up templating engine
-        var exphbs = require('express-handlebars');
-        var hbs = exphbs.create({
-    helpers: {
-        compare: function (lvalue, rvalue, options) { 
-            if (arguments.length < 3)
+         var exphbs = require('express-handlebars');
+       var hbs = require('hbs');
+
+        hbs.registerHelper('compare', function(lvalue, rvalue, options) {
+
+        if (arguments.length < 3)
             throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
 
-            var operator = options.hash.operator || "==";
-
-            var operators = {
-             '==':       function(l,r) { return l == r; },
-            '===':      function(l,r) { return l === r; },
+        var operator = options.hash.operator || "==";
+        
+        var operators = {
+            '==':       function(l,r) { return l == r; },
+            '===':  function(l,r) { return l === r; },
             '!=':       function(l,r) { return l != r; },
             '<':        function(l,r) { return l < r; },
             '>':        function(l,r) { return l > r; },
             '<=':       function(l,r) { return l <= r; },
             '>=':       function(l,r) { return l >= r; },
             'typeof':   function(l,r) { return typeof l == r; }
-            }
+        }
 
-         if (!operators[operator])
-                throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
+        if (!operators[operator])
+            throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
 
-            var result = operators[operator](lvalue,rvalue);
+        var result = operators[operator](lvalue,rvalue);
 
-            if( result ) {
-                return options.fn(this);
-                } else {
+        if( result ) {
+            return options.fn(this);
+        } else {
             return options.inverse(this);
-           }
-        } 
-    }
+        }
+        
     });
+app.set('view engine', 'html');
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('html', require('hbs').__express, exphbs({ defaultLayout: 'main' }));
+    
 
-    app.engine('handlebars', hbs.engine);
-     app.engine('html', exphbs({ defaultLayout: 'main' }));
-        app.set('view engine', 'html');
-        app.set('views', path.join(__dirname, 'views'));
+    //     var hbs = exphbs.create({
+    // helpers: {
+    //     compare: function (lvalue, rvalue, options) { 
+    //         if (arguments.length < 3)
+    //         throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+
+    //         var operator = options.hash.operator || "==";
+
+    //         var operators = {
+    //          '==':       function(l,r) { return l == r; },
+    //         '===':      function(l,r) { return l === r; },
+    //         '!=':       function(l,r) { return l != r; },
+    //         '<':        function(l,r) { return l < r; },
+    //         '>':        function(l,r) { return l > r; },
+    //         '<=':       function(l,r) { return l <= r; },
+    //         '>=':       function(l,r) { return l >= r; },
+    //         'typeof':   function(l,r) { return typeof l == r; }
+    //         }
+
+    //      if (!operators[operator])
+    //             throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
+
+    //         var result = operators[operator](lvalue,rvalue);
+
+    //         if( result ) {
+    //             return options.fn(this);
+    //             } else {
+    //         return options.inverse(this);
+    //        }
+    //     } 
+    // }
+    // });
+
+    // app.engine('handlebars', hbs.engine);
+    //  app.engine('html', exphbs({ defaultLayout: 'main' }));
+    //     app.set('view engine', 'html');
+    //     app.set('views', path.join(__dirname, 'views'));
+
         // uncomment after placing your favicon in /public
         //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
         app.use(logger('dev'));
